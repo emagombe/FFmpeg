a1368485c856e08ceb754c9d6ec5afe9b0bdc14e
Edson Magombe <edson.patricio.39@gmail.com>
2023-11-22 00:20:58 +0200

    Added audio formats

--- ffmpeg-build.sh
+++ ffmpeg-build.sh
@@ -63,12 +63,12 @@ function configure_ffmpeg {
     --disable-static \
     ${EXTRA_CONFIG} \
     --disable-everything \
-    --enable-parser=aac,wav,h264,mp3,mp4,ogg,mov,mkv,flac,mpeg4,vorbis,opus,webm,pcm \
-    --enable-demuxer=aac,wav,h264,mp3,mp4,ogg,mov,mkv,flac,mpeg4,vorbis,opus,webm,pcm \
-    --enable-decoder=aac,wav,h264,mp3,mp4,ogg,mov,mkv,flac,mpeg4,vorbis,opus,webm,pcm \
+    --enable-parser=aac,mpegaudio,gsm,opus,flac \
+    --enable-demuxer=flac,tak,dsf,iff,mpc,mpc8,wav,flv,mp3,mov,asf,gsm,aiff,ape,aac,ogg,tta,pcm_s8,pcm_u8,pcm_s16be,pcm_s16le,pcm_u16be,pcm_u16le,pcm_u16be,pcm_u24be,pcm_u24le,pcm_s24be,pcm_s24le,pcm_u32be,pcm_u32le,pcm_s32be,pcm_s32le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_alaw,pcm_mulaw \
+    --enable-decoder=aac,mp3float,mp2float,mp3on4float,mp3adufloat,vorbis,wmav1,wmav2,alac,mace3,mace6,wmapro,wmalossless,ape,tta,flac,opus,tak,pcm_s8,pcm_s8_planar,pcm_s16be,pcm_s16le,pcm_u16be,pcm_u16le,pcm_s16be_planar,pcm_s16le_planar,pcm_f64le,pcm_f64be,pcm_s24be,pcm_s24daud,pcm_s24le,pcm_s24le_planar,pcm_u24be,pcm_u24le,pcm_s32be,pcm_s32le,pcm_u32be,pcm_u32le,pcm_f32be,pcm_f32le,pcm_s32le_planar,pcm_alaw,pcm_mulaw,adpcm_ms,adpcm_g726,gsm,gsm_ms,adpcm_ima_qt,adpcm_4xm,adpcm_adx,adpcm_ct,adpcm_ea,adpcm_ea_maxis_xa,adpcm_ea_r1,adpcm_ea_r2,adpcm_ea_r3,adpcm_ea_xas,adpcm_ima_amv,adpcm_ima_dk3,adpcm_ima_dk4,adpcm_ima_ea_eacs,adpcm_ima_ea_sead,adpcm_ima_iss,adpcm_ima_smjpeg,adpcm_ima_wav,adpcm_ima_ws,adpcm_swf,adpcm_xa,adpcm_yamaha,pcm_zork,pcm_bluray,pcm_lxf,pcm_dvd \
     --enable-protocol=file \
     --enable-protocol=concat \
-    --enable-protocol=libvpx \
+    --enable-libvpx \
     --disable-avdevice \
     --enable-avcodec \
     --enable-avformat \
@@ -114,6 +114,13 @@ function build_ffmpeg {
 }
 
 build_ffmpeg armeabi-v7a 22
+cp ./build/armeabi-v7a/lib/* F:\\ANDROID\\PROJECTS\\beatlevelsplayer\\app\\libs\\armeabi-v7a
+
 build_ffmpeg arm64-v8a 22
+cp ./build/arm64-v8a/lib/* F:\\ANDROID\\PROJECTS\\beatlevelsplayer\\app\\libs\\arm64-v8a
+
 build_ffmpeg x86 22
+cp ./build/x86/lib/* F:\\ANDROID\\PROJECTS\\beatlevelsplayer\\app\\libs\\x86
+
 build_ffmpeg x86_64 22
+cp ./build/x86_64/lib/* F:\\ANDROID\\PROJECTS\\beatlevelsplayer\\app\\libs\\x86_64
